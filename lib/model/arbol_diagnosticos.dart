9d14f5d5f0e67fd617d5c9a447ceef77b44c6b3f
Claudio Grasso <claudio.grasso@yahoo.com>
2023-04-02 20:37:08 -0300

    Corrijo el árbol de diagnósitcos de ejemplo (le faltaban algunas ramas en los niveles superiores) Agrego el archivo test_data.dart que no sé para qué lo puse pero, por las dudas...

--- lib/model/arbol_diagnosticos.dart
+++ lib/model/arbol_diagnosticos.dart
@@ -1,4 +1,3 @@
-
 var diag1 = ['Diagnóstico 1', 'Diagnóstico 2', 'Diagnóstico 3'];
 
 var diag2 = [
@@ -30,13 +29,33 @@ var diag3 = [
   // Ramas Diagnostico 3
   [
     // Rama Diagnostico 31
-    ['Diagnóstico 311'],
+    [
+      'Diagnóstico 311',
+      'Diagnóstico 312',
+      'Diagnóstico 313',
+      'Diagnóstico 314',
+    ],
     // Rama Diagnostico 32
-    ['Diagnóstico 321'],
+    [
+      'Diagnóstico 321',
+      'Diagnóstico 322',
+      'Diagnóstico 323',
+      'Diagnóstico 324',
+    ],
     // Rama Diagnostico 33
-    ['Diagnóstico 331'],
+    [
+      'Diagnóstico 331',
+      'Diagnóstico 332',
+      'Diagnóstico 333',
+      'Diagnóstico 334',
+    ],
     // Rama Diagnostico 34
-    ['Diagnóstico 341'],
+    [
+      'Diagnóstico 341',
+      'Diagnóstico 342',
+      'Diagnóstico 343',
+      'Diagnóstico 344',
+    ],
   ]
 ];
 
@@ -56,6 +75,19 @@ var diag4 = [
       ['Diagnóstico 1211', 'Diagnóstico 1212'],
       // Ramas diagnóstico 122
       ['Diagnóstico 1221', 'Diagnóstico 1222']
+    ],
+    // Ramas diagnóstico 13
+    [
+      // Ramas diagnóstico 131
+      [
+        'Diagnóstico 1311',
+      ],
+      // Ramas diagnóstico 132
+      [
+        'Diagnóstico 1321',
+      ],
+      // Ramas diagnóstico 133
+      ['Diagnóstico 1331', 'Diagnóstico 1332']
     ]
   ],
   // Ramas diagnóstico 2
@@ -123,3 +155,57 @@ var diag4 = [
     ],
   ]
 ]; // Fin diag4
+
+List<List<List<List<String>>>> nivel4 = [
+  [
+    [
+      ['Diagnóstico 1111', 'Diagnóstico 1112'],
+      ['Diagnóstico 1121', 'Diagnóstico 1122'],
+      ['Diagnóstico 1131', 'Diagnóstico 1132', 'Diagnóstico 1133']
+    ],
+    [
+      ['Diagnóstico 1211', 'Diagnóstico 1212'],
+      ['Diagnóstico 1221', 'Diagnóstico 1222'],
+      ['Diagnóstico 1231', 'Diagnóstico 1232', 'Diagnóstico 1233']
+    ],
+    [
+      ['Diagnóstico 1311', 'Diagnóstico 1312'],
+      ['Diagnóstico 1321', 'Diagnóstico 1322'],
+      ['Diagnóstico 1331', 'Diagnóstico 1332', 'Diagnóstico 1333']
+    ],
+  ],
+  [
+    [
+      ['Diagnóstico 2111', 'Diagnóstico 2112'],
+      ['Diagnóstico 2121', 'Diagnóstico 2122'],
+      ['Diagnóstico 2131', 'Diagnóstico 2132', 'Diagnóstico 2133']
+    ],
+    [
+      ['Diagnóstico 2211', 'Diagnóstico 2212'],
+      ['Diagnóstico 2221', 'Diagnóstico 2222'],
+      ['Diagnóstico 2231', 'Diagnóstico 2232', 'Diagnóstico 2233']
+    ],
+    [
+      ['Diagnóstico 2311', 'Diagnóstico 2312'],
+      ['Diagnóstico 2321', 'Diagnóstico 2322'],
+      ['Diagnóstico 2331', 'Diagnóstico 2332', 'Diagnóstico 2333']
+    ],
+  ],
+  [
+    [
+      ['Diagnóstico 3111', 'Diagnóstico 3112'],
+      ['Diagnóstico 3121', 'Diagnóstico 3122'],
+      ['Diagnóstico 3131', 'Diagnóstico 3132', 'Diagnóstico 3133']
+    ],
+    [
+      ['Diagnóstico 3211', 'Diagnóstico 3212'],
+      ['Diagnóstico 3221', 'Diagnóstico 3222'],
+      ['Diagnóstico 3231', 'Diagnóstico 3232', 'Diagnóstico 3233']
+    ],
+    [
+      ['Diagnóstico 3311', 'Diagnóstico 3312'],
+      ['Diagnóstico 3321', 'Diagnóstico 3322'],
+      ['Diagnóstico 3331', 'Diagnóstico 3332', 'Diagnóstico 3333']
+    ],
+  ],
+];
---
+++ lib/model/test_data.dart
@@ -0,0 +1,116 @@
+var diag4 = [
+  [
+    [
+      [
+        'Diagnóstico 1111',
+        'Diagnóstico 1112',
+      ],
+      [
+        'Diagnóstico 1121',
+        'Diagnóstico 1122',
+      ]
+    ],
+    [
+      [
+        'Diagnóstico 1211',
+        'Diagnóstico 1212',
+      ],
+      [
+        'Diagnóstico 1221',
+        'Diagnóstico 1222',
+      ]
+    ],
+    [
+      [
+        'Diagnóstico 1311',
+      ],
+      [
+        'Diagnóstico 1321',
+      ],
+      [
+        'Diagnóstico 1331',
+        'Diagnóstico 1332',
+      ]
+    ]
+  ],
+  [
+    [
+      [
+        'Diagnóstico 2111',
+        'Diagnóstico 2112',
+      ],
+      [
+        'Diagnóstico 2121',
+        'Diagnóstico 2122',
+      ]
+    ],
+    [
+      [
+        'Diagnóstico 2211',
+        'Diagnóstico 2212',
+      ],
+      [
+        'Diagnóstico 2221',
+        'Diagnóstico 2222',
+      ]
+    ]
+  ],
+  [
+    [
+      [
+        'Diagnóstico 3111',
+      ],
+      [
+        'Diagnóstico 3121',
+      ],
+      [
+        'Diagnóstico 3131',
+      ],
+      [
+        'Diagnóstico 3141',
+      ],
+    ],
+    [
+      [
+        'Diagnóstico 3211',
+      ],
+      [
+        'Diagnóstico 3221',
+      ],
+      [
+        'Diagnóstico 3231',
+      ],
+      [
+        'Diagnóstico 3241',
+      ],
+    ],
+    [
+      [
+        'Diagnóstico 3311',
+      ],
+      [
+        'Diagnóstico 3321',
+      ],
+      [
+        'Diagnóstico 3331',
+      ],
+      [
+        'Diagnóstico 3341',
+      ],
+    ],
+    [
+      [
+        'Diagnóstico 3411',
+      ],
+      [
+        'Diagnóstico 3421',
+      ],
+      [
+        'Diagnóstico 3431',
+      ],
+      [
+        'Diagnóstico 3441',
+      ],
+    ],
+  ]
+];
