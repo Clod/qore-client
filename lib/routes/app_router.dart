9f2e5bb5b84e8fca6ef5a0f7ccbb5fcc8f7fd057
Claudio Grasso <claudio.grasso@yahoo.com>
2023-08-01 13:24:47 -0300

    Solo agrego unos comentarios pero hago un commit porque voy a hacer un experimento potencialmente destructivo.

--- lib/routes/app_router.dart
+++ lib/routes/app_router.dart
@@ -12,8 +12,6 @@ import 'package:flutter/foundation.dart';
 
 import '../util/auth_service.dart';
 
-
-
 // https://pub.dev/packages/auto_route#wrapping-routes
 /*
 @MaterialAutoRouter(
@@ -55,7 +53,7 @@ import '../util/auth_service.dart';
       path: 'edit_patient',
     )
 */
-    // All routes within Dashboard are guarded
+// All routes within Dashboard are guarded
 /*    AutoRoute(
       page: DashboardScreen,
       name: 'DashboardRoute',
@@ -88,7 +86,7 @@ import '../util/auth_service.dart';
             path: 'edit_patient')
       ],
     ),*/
-    // AutoRoute(page: AboutScreen, name: 'AboutRouter', path: '/about')
+// AutoRoute(page: AboutScreen, name: 'AboutRouter', path: '/about')
 //   ],
 // )
 // class $AppRouter {
@@ -97,11 +95,13 @@ import '../util/auth_service.dart';
 // Clod: Después de armar este archivo se corre
 // D:\flutter\bin\flutter packages pub run build_runner build --delete-conflicting-outputs
 
+// The generated file app_router.gr.dat has a
+// part of 'app_router.dart' sentence
 part 'app_router.gr.dart';
 
+// _$AppRouter is also genrated by the tool in said part
 @AutoRouterConfig()
 class AppRouter extends _$AppRouter {
-
   final AuthService authService;
 
   AppRouter(this.authService);
@@ -111,48 +111,49 @@ class AppRouter extends _$AppRouter {
 
   @override
   List<AutoRoute> get routes => [
-    AutoRoute(page: LoginRoute.page),
-    AutoRoute(page: HomeRoute.page),
-    AutoRoute(page: PatientsRoute.page, guards:[AuthGuard(authService)]),
-    AutoRoute(page: AddPatientRoute.page),
-    AutoRoute(page: EditPatientRoute.page),
-    AutoRoute(page: AboutRoute.page),
-    /// routes go here
-  // AutoRoute(
-  // page: LoginScreen,
-  // name: 'LoginRoute',
-  // path: 'login',
-  // ),
-  // AutoRoute(
-  // page: HomeScreen,
-  // name: 'HomeRoute',
-  // path: '/',
-  // ),
-  // AutoRoute(
-  // page: PatientsScreen,
-  // name: 'PatientsRoute',
-  // path: 'patients',
-  // // guards: kReleaseMode ? [RouteGuard] : null,
-  // guards: [RouteGuard],
-  // ),
-  // AutoRoute(
-  // page: AddPatientScreen,
-  // name: 'AddPatientRoute',
-  // path: 'add_patient',
-  // //guards: kReleaseMode ? [RouteGuard] : null,
-  // guards: [RouteGuard],
-  // ),
-  // AutoRoute(
-  // page: AboutScreen,
-  // name: 'AboutRouter',
-  // path: '/about',
-  // ),
-  // AutoRoute(
-  // page: EditPatientScreen,
-  // name: 'EditPatientRoute',
-  // // guards: kReleaseMode ? [RouteGuard] : null,
-  // guards: [RouteGuard],
-  // path: 'edit_patient',
-  // )
-  ];
+        AutoRoute(page: LoginRoute.page),
+        AutoRoute(page: HomeRoute.page),
+        AutoRoute(page: PatientsRoute.page, guards: [AuthGuard(authService)]),
+        AutoRoute(page: AddPatientRoute.page, guards: [AuthGuard(authService)]),
+        AutoRoute(page: EditPatientRoute.page, guards: [AuthGuard(authService)]),
+        AutoRoute(page: AboutRoute.page),
+
+        /// routes go here
+        // AutoRoute(
+        // page: LoginScreen,
+        // name: 'LoginRoute',
+        // path: 'login',
+        // ),
+        // AutoRoute(
+        // page: HomeScreen,
+        // name: 'HomeRoute',
+        // path: '/',
+        // ),
+        // AutoRoute(
+        // page: PatientsScreen,
+        // name: 'PatientsRoute',
+        // path: 'patients',
+        // // guards: kReleaseMode ? [RouteGuard] : null,
+        // guards: [RouteGuard],
+        // ),
+        // AutoRoute(
+        // page: AddPatientScreen,
+        // name: 'AddPatientRoute',
+        // path: 'add_patient',
+        // //guards: kReleaseMode ? [RouteGuard] : null,
+        // guards: [RouteGuard],
+        // ),
+        // AutoRoute(
+        // page: AboutScreen,
+        // name: 'AboutRouter',
+        // path: '/about',
+        // ),
+        // AutoRoute(
+        // page: EditPatientScreen,
+        // name: 'EditPatientRoute',
+        // // guards: kReleaseMode ? [RouteGuard] : null,
+        // guards: [RouteGuard],
+        // path: 'edit_patient',
+        // )
+      ];
 }
